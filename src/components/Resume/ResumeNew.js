d82a318a384981894dd007fea09626321e0326ec
unknown <biruktawitaberaa@gmail.com>
2025-03-31 15:17:49 +0300

    Resume added

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/BiruktawitAberaResume.pdf";
+import pdf from "../../Assets/../Assets/Biruktaiwt-Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/BiruktawitAbera/Resume/main/BiruktawitAberaResume.pdf";
+  "https://raw.githubusercontent.com/BiruktawitAbera/Resume/main/Biruktaiwt-Resume.pdf";
   
 
 function ResumeNew() {
