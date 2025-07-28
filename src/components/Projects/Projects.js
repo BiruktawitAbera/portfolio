b970ae6da980ea0be3294c23c61c5fb00a187878
unknown <biruktawitaberaa@gmail.com>
2025-07-28 14:09:14 +0300

    Add 2 projects of figma design

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,8 @@ import ecommerce from "../../Assets/Projects/ecommerce.png";
 import budgetwise from "../../Assets/Projects/budgetwise.jpg";
 import crm from "../../Assets/Projects/crm.jpg";
 import foodrecipe from "../../Assets/Projects/foodrecipe.PNG";
+import Ecommercee from "../../Assets/Projects/Ecommercee.PNG";
+import Saas from "../../Assets/Projects/Saas.PNG";
 
 function Projects() {
   return (
@@ -23,8 +25,8 @@ function Projects() {
             <ProjectCard
               imgPath={budgetwise}
               isBlog={false}
-              title="budget-wise"
-              description="organizational finance tracking web app "
+              title="Budget-Wise"
+              description="Organizational finance tracking web app"
               demoLink="https://github.com/BiruktawitAbera/Organizational_Finance_Tracking_Front_end.git"
             />
           </Col>
@@ -33,8 +35,8 @@ function Projects() {
             <ProjectCard
               imgPath={crm}
               isBlog={false}
-              title="crm"
-              description="A Human resource management system for Xcel tech campany"
+              title="CRM"
+              description="Human resource management system for Xcel Tech"
               demoLink="https://github.com/BiruktawitAbera/crm.git"
             />
           </Col>
@@ -44,7 +46,7 @@ function Projects() {
               imgPath={ecommerce}
               isBlog={false}
               title="E-commerce"
-              description="Online cloth shopping"
+              description="Online cloth shopping platform"
               demoLink="https://github.com/BiruktawitAbera/Online-cloth-shopping.git"              
             />
           </Col>
@@ -53,14 +55,33 @@ function Projects() {
             <ProjectCard
               imgPath={foodrecipe}
               isBlog={false}
-              title="UI/UX Designs"
-              description="A collection of my UI/UX designs"
-              demoLink="https://github.com/BiruktawitAbera/food-recipe-design/raw/main/Food-Recipe.pdf
-" // Replace with the actual path
+              title="Food Recipe App"
+              description="UI/UX design for recipe application"
+              demoLink="https://github.com/BiruktawitAbera/food-recipe-design/raw/main/Food-Recipe.pdf"
+            />
+          </Col>
+          
+          {/* Figma Project 1 */}
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Ecommercee}
+              isBlog={false}
+              title="Data Wise"
+              description="Figma design for Ecommerce website"
+              demoLink="https://www.figma.com/design/YHLgBTsyGym811F5c3XCbe/Data-Wise?node-id=0-1&t=veCucmOHWXlraEIS-1"
             />
           </Col>
 
-          
+          {/* Figma Project 2 */}
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={Saas}
+              isBlog={false}
+              title="Saas Dashboard"
+              description="UI design for SaaS platform"
+              demoLink="https://www.figma.com/design/3jcVNyldjP9urtracEtcSD/Ecommerce-website?node-id=0-1&t=9gb3qbwmHPp6Jppg-1"
+            />
+          </Col>
         </Row>
       </Container>
     </Container>
